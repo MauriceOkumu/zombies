c236b989aec29a38023ceec39d54565471b93fb5
Maurice Okumu <maurice@Maurices-MacBook-Pro.local>
2023-01-29 20:46:20 -0600

    refactor zombie chase range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Header("Zombie behaviour and movement")]
+	[Tooltip("Zombie will move and attack this target")]
+	[SerializeField] Transform target;
+	[Tooltip("The chase range of the zombie")]
+	[SerializeField] float chaseRange = 10f;
+	[Tooltip("The distance to the target")]
+	[SerializeField] float targetDistance = Mathf.Infinity;
+	 UnityEngine.AI.NavMeshAgent agent;
+	void Start()
+	{
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		targetDistance = Vector3.Distance(target.position, transform.position);
+		
+		//See if the player is in attack range
+		if(targetDistance <= chaseRange) agent.SetDestination(target.position);
+	}
+	
 }
